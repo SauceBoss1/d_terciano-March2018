54d48e3d633ead9c030ac7146d9745bfe792e948
SauceBoss1 <dtvsworld@gmail.com>
2018-03-23 08:49:29 -0700

    TextExcel

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -9,15 +9,21 @@ public class FormulaCell extends RealCell {
 	
 	public double getDoubleValue() {
 		String[] userFormula = super.getUserInput().substring(2, getUserInput().length()-2).split(" ");
-		if(userFormula[0].isDigit())
+		char[] userFormula1 = toChar(userFormula);
+		for(int i = 0 ; i<userFormula1.length; i++) {
+			if(Character.isDigit(userFormula1[i])) {
+				
+			}
+		}
 		return 0;
 	}
 	
 	public char[] toChar(String[] input) {
-		for(int i ; i < input.length; i++) {
-			
+		char[] result = new char[input.length];
+		for(int i = 0; i < input.length; i++) {
+			result[i]= input[i].charAt(0);
 		}
-		return '0';
+		return result;
 	}
 	
 	
